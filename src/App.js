eb29c9f1b76c726bcb82f24080fa53f4c6f8bb18
awesomecoder233 <tommount58@gmail.com>
2021-07-23 15:41:20 +0200

    create react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     // ...
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tutorials" className="navbar-brand">
+            Arman
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                add
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div>
+          <switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            <Route exact path="/add" component={AddTutorial} />
+            <Route exact path="/tutorials/:id" component={Tutorial} />
+          </switch>
+        </div>
+      </div>
+    );
   }
 }
 
